82d90b10e1a896d27147b1c4cc67c7fecc329996
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-09-22 12:49:53 +1200

    fixed html and script 1 typos

--- script/script1.js
+++ script/script1.js
@@ -1,6 +1,6 @@
 function createWindow() {
     let url = "https://google.com";
     let win = window.open(url, "My New Window", "Width=300", "Height=200");
-    document.getElementById("result").innderHTML = 
+    document.getElementById("result").innerHTML = 
         win.name + " - " + win.opener.location;
 }
